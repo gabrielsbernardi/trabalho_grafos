41bffeef6cdcd83daf15b8c0bb0a15ffc56f91ee
Gabriel Bernardi <Gabriel Bernardi@DESKTOP-FLEF9V5>
2016-09-04 19:10:30 -0300

    Classes Iniciais

---
+++ Exercicios/src/exe6/ValidarGrafo.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exe6;
+
+/**
+ *
+ * @author Gabriel Bernardi
+ */
+public class ValidarGrafo {
+    private int[][] grafo;
+    
+    public ValidarGrafo(int[][] grafo) {
+        this.grafo = grafo;
+    } 
+    
+    /**
+     * 
+     * @return String contendo o tipo do grafo
+     */
+    public String tipoDoGrafo(){
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("O grafo informado contém as seguinte caracteríticas: ")
+        .append(this.isDirigido())
+        .append(", ")
+        .append(this.isSimples())
+        .append(", ")
+        .append(this.isRegular())
+        .append(", ")
+        .append(this.isCompleto())
+        .append(" e ")
+        .append(this.isNulo())
+        .append(".");
+        return retorno.toString();
+    }
+    
+    private String isDirigido(){
+        return "";
+    }
+    
+    private String isSimples(){
+        return "";
+    }
+    
+    private String isRegular(){
+        return "";
+    }
+    
+    private String isCompleto(){
+        return "";
+    }
+    private String isNulo(){
+        return "";
+    }
+    
+    /**
+     * 
+     * @return String com a quantidade e o conjunto de arestas
+     */
+    public String arestasDoGrafo(){
+        return "";
+    }
+    
+    /**
+     * 
+     * @return String identificando o grau de cada vértice e por fim, a sequência de graus
+     */
+    public String grausDoVertice(){
+        return "";
+    }
+}
