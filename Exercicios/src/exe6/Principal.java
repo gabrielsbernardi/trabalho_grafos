ab5e0f4a93b622c3f4f15e7bbb5a6b64a4575287
flaviolosada <>
2016-09-07 21:15:19 -0300

    Quase

--- Exercicios/src/exe6/Principal.java
+++ Exercicios/src/exe6/Principal.java
@@ -12,17 +12,58 @@ import java.util.Scanner;
  * @author Flávio Omar Losada e Gabriel da Silva Bernardi
  */
 public class Principal {
-    private static Scanner ler = new Scanner(System.in);
-    private static ValidarGrafo vg;
+    private static int[][] matrizAdjacencia;
+    private static int maxLinhas = 0;
     
     public static void main(String[] args) {
-        System.out.print("Informe a ordem do grafo: ");
-        int ordem = ler.nextInt();
-        vg = new ValidarGrafo(new int[ordem][ordem]);
-        informarArestas();
+        informarArestasPorLinha();
     }
 
-    private static void informarArestas() {
-        
+    private static void informarArestasPorLinha() {
+        String valorLinha = "";
+        int numLinha = 0;
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Separe os valores da linha por espaço. Pressione \"Enter\" para cada nova linha da matriz.");
+        System.out.println("Informe @ para parar de digitar.");
+        do {
+            System.out.print("Linha " + numLinha + ": ");
+            valorLinha = ler.nextLine();
+            if ("".equals(valorLinha.trim())) {
+                System.err.println("Valor inválido!\n");
+            } else {
+                processarLinha(valorLinha, numLinha);
+                numLinha++;
+            }
+        } while (maxLinhas > numLinha);
+        ValidarGrafo grafo = new ValidarGrafo();
+        System.out.println(grafo.tipoDoGrafo(matrizAdjacencia) 
+                            + "\n" + grafo.arestasDoGrafo(matrizAdjacencia)
+                            + "\n" + grafo.grausDoVertice(matrizAdjacencia));
     }
+    
+    private static void processarLinha(String linha, int numLinha){
+        String[] valoresLinha = linha.split(" ");
+        if (numLinha == 0) {
+            matrizAdjacencia = new int[valoresLinha.length][valoresLinha.length];
+            maxLinhas = valoresLinha.length;
+        } 
+        for (int i = 0; i < valoresLinha.length; i++) {
+            if (isNumber(valoresLinha[i])) {
+                matrizAdjacencia[numLinha][i] = Integer.parseInt(valoresLinha[i]);
+            } else {
+                System.err.println("Valor inválido na linha " + numLinha);
+                System.exit(0);
+            }
+        }
+    }
+    
+    private static boolean isNumber(String value) {
+        try {
+            Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+        
 }
--- Exercicios/src/exe6/ValidarGrafo.java
+++ Exercicios/src/exe6/ValidarGrafo.java
@@ -5,24 +5,27 @@
  */
 package exe6;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Flávio Omar Losada e Gabriel da Silva Bernardi
  */
 public class ValidarGrafo {
     private int[][] grafo;
+    private boolean dirigido;
     
-    public ValidarGrafo(int[][] grafo) {
-        this.grafo = grafo;
-    } 
+    public ValidarGrafo() {} 
     
     /**
      * 
      * @return String contendo o tipo do grafo
      */
-    public String tipoDoGrafo(){
+    public String tipoDoGrafo(int[][] grafo){
+        this.grafo = grafo;
         StringBuilder retorno = new StringBuilder();
-        retorno.append("O grafo informado contém as seguinte caracteríticas: ")
+        
+        retorno.append("Tipo do Grafo: ")
         .append(this.isDirigido())
         .append(", ")
         .append(this.isSimples())
@@ -30,53 +33,200 @@ public class ValidarGrafo {
         .append(this.isRegular())
         .append(", ")
         .append(this.isCompleto())
-        .append(" e ")
+        .append(", ")
         .append(this.isNulo())
+        .append(" e ")
+        .append(this.isBipartido())
         .append(".");
         return retorno.toString();
     }
     
-    private String isDirigido(){
-        return "";
+    private String isDirigido() {
+        String linha = "";
+        String coluna = "";
+        for (int j = 0; j < grafo.length; j++) {
+            for (int i = 0; i < grafo[j].length; i++) {
+                linha += grafo[j][i];
+            }
+            for (int i = 0; i < grafo.length; i++) {
+                coluna += grafo[i][j];
+            }
+        }
+        if (linha.equals(coluna)) {
+            this.dirigido = false;
+            return "Não dirigido";
+        } else {
+            this.dirigido = true;
+            return "Dirigido";
+        }
     }
     
-    private String isSimples(){
-        return "";
+    private String isSimples() {
+        for (int i = 0; i < grafo.length; i++) {
+            if (grafo[i][i] != 0) {
+                return "multigrafo";
+            }
+        }
+        for (int i = 0; i < grafo.length; i++) {
+            for (int j = 0; j < grafo[i].length; j++) {
+                if (grafo[i][j] > 1) {
+                    return "multigrafo";
+                }
+            }
+        }
+        return "simples";
     }
     
-    private String isRegular(){
-        return "";
+    private String isRegular() {
+        int valorAnterior = 0;
+        int valorAtual = 0;
+        for (int i = 0; i < grafo.length; i++) {
+            for (int j = 0; j < grafo[i].length; j++) {
+                valorAtual += grafo[i][j];
+            }
+            if (i == 0) {
+                valorAnterior = valorAtual;
+            }
+            if (valorAnterior != valorAtual) {
+                return "não regular";
+            } 
+            valorAtual = 0;
+        }
+        return "regular";
     }
     
     private String isCompleto(){
-        return "";
+        for (int i = 0; i < grafo.length; i++) {
+            for (int j = 0; j < grafo[i].length; j++) {
+                if (i == j) {
+                    if (grafo[i][j] != 0) {
+                        return "não completo";
+                    }
+                } else {
+                    if (grafo[i][j] != 1) {
+                        return "não completo";
+                    }
+                }
+            }
+        }
+        return "completo";
     }
-    private String isNulo(){
-        return "";
+    
+    private String isNulo() {
+        for (int i = 0; i < grafo.length; i++) {
+            for (int j = 0; j < grafo[i].length; j++) {
+                if (grafo[i][j] != 0) {
+                    return "não nulo";
+                }
+            }
+        }
+        return "nulo";
+    }
+    
+    private String isBipartido() {
+        ArrayList<Integer> grupoA = new ArrayList<Integer>();
+        ArrayList<Integer> grupoB = new ArrayList<Integer>();
+        for (int i = 0; i < grafo.length; i++) {
+            for (int j = 0; j < grafo[i].length; j++) {
+                if (i == j && grafo[i][j] > 0) {
+                    return "não bipartido";
+                }
+                if (grafo[i][j] > 0) {
+                    if (!adicionaIndice(grupoA, grupoB, i)) {
+                        return "não bipartido";
+                    }
+                    if (!adicionaIndice(grupoB, grupoA, j)) {
+                        return "não bipartido";
+                    }
+                }
+            }
+        }
+        return "bipartido";
+    } 
+    
+    private boolean adicionaIndice(ArrayList<Integer> grupo1, ArrayList<Integer> grupo2, int indice) {
+        boolean inseridoSucesso = true;
+        if (grupo1.size() == 0) {
+            grupo1.add(indice);
+        } else {
+            for (Integer indiceLista : grupo1) {
+                if (grafo[indiceLista][indice] > 0) {
+                    inseridoSucesso = false;
+                    break;
+                }          
+            }
+            if (!grupo1.contains(indice) && inseridoSucesso) {
+                grupo1.add(indice);
+            }
+        }
+        if (!inseridoSucesso) {
+            if (grupo2.size() == 0) {
+                grupo2.add(indice);
+            } else {
+                for (Integer indiceLista : grupo2) {
+                    if (grafo[indiceLista][indice] > 0) {
+                        return false;
+                    }
+                }
+                if (!grupo2.contains(indice)) {
+                    grupo2.add(indice);
+                }
+            }
+        }
+        return true;
     }
     
     /**
      * 
      * @return String com a quantidade e o conjunto de arestas
      */
-    public String arestasDoGrafo(){
-        return "";
+    public String arestasDoGrafo(int[][] grafo){
+        this.grafo = grafo;
+        int qtdArestas = 0;
+        String conjunto = "";
+        
+        for (int i = 0; i < grafo.length; i++) {
+            for (int j = 0; j < grafo[i].length; j++) {
+                qtdArestas += grafo[i][j];
+                if (grafo[i][j] > 0) {
+                    conjunto += "(" + i + ", " + j + "), ";
+                }
+            }
+        }
+        conjunto = conjunto.substring(0, conjunto.length()-2);
+        if (dirigido){
+            return "Quantidade de arestas: " + qtdArestas + "\nConjunto de arestas: E = {" + conjunto + "}";
+        }
+        return "Quantidade de arestas: " + (qtdArestas / 2) + "\nConjunto de arestas: E = {" + conjunto + "}";
     }
     
     /**
      * 
      * @return String identificando o grau de cada vértice e por fim, a sequência de graus
      */
-    public String grausDoVertice(){
-        return "";
-    }
-    
-    private void percorrerGrafo(){
-        for (int linha = 0; linha < grafo.length; linha++) {
-            for (int coluna = 0; coluna < grafo.length; coluna++) {
-//                int[] is = grafo[coluna];
+    public String grausDoVertice(int[][] grafo){
+        this.grafo = grafo;
+        String grauVertice = "";
+        ArrayList<Integer> lstGraus = new ArrayList<Integer>();
+        int qtdGraus = 0;
+        
+        for (int i = 0; i < grafo.length; i++) {
+            for (int j = 0; j < grafo[i].length; j++) {
+                qtdGraus += grafo[i][j];
             }
-//            int[] is = grafo[linha];
+            grauVertice += "V" + i + ": " + qtdGraus + ", ";
+            lstGraus.add(qtdGraus);
+            qtdGraus = 0;
         }
+        grauVertice = grauVertice.substring(0, grauVertice.length()-2);
+        grauVertice += "\nSequência dos graus: ";
+        lstGraus.sort(null);
+        
+        for (Integer lstGrau : lstGraus) {
+            grauVertice += lstGrau + ", ";
+        }
+        grauVertice = grauVertice.substring(0, grauVertice.length()-2);
+        
+        return "Grau de cada vértice: " + grauVertice;
     }
 }
